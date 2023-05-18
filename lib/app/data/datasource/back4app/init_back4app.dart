c6c6ae461d62b57410c68b9ccd721211cd99dae5
Catalunha <catalunha.mj@gmail.com>
2023-05-18 18:06:03 -0300

    Update init_back4app.dart

--- lib/app/data/datasource/back4app/init_back4app.dart
+++ lib/app/data/datasource/back4app/init_back4app.dart
@@ -7,8 +7,8 @@ class InitBack4app {
 
   Future<bool> init() async {
     const keyParseServerUrl = 'https://parseapi.back4app.com';
-    const keyApplicationId = 'ZuF4FI4dZUN4i9ObIoK6LvTuoIuVFNsVAMRjYNkX';
-    const keyClientKey = 'o95t2IrCn5PNbgPzomQ1RSAwB2iob1jmOuhYfL0T';
+    const keyApplicationId = '';
+    const keyClientKey = '';
     const keyLiveQueryUrl = 'https://cemecaluno.b4a.io';
     await Parse().initialize(
       keyApplicationId,
